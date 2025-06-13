b57f29b6a449c587541b71c41e740d1dca77eb91
zanintt <tiago.zanint@gmail>
2025-06-12 22:19:14 -0300

    objetos no js

---
+++ objetos/exemplo_objeto.js
@@ -0,0 +1,12 @@
+const aluno = {
+    nome:"tiago",
+    idade: 23,
+    curço:"Desenvolvimento de Sistema"
+};
+
+console.log(aluno.nome);
+console.log(aluno["idade"]);
+
+aluno.matricula = "2025A";
+aluno.idade =18;
+console.log(aluno);
